520fee140bd91d89b3f6e137968ec7eb79f5354d
Tudor <tudor@Tudors-MacBook-Pro.local>
2025-09-24 23:06:58 +0300

    Add url_launcher dependency for Terms and Privacy links
    - Added url_launcher package to pubspec.yaml
    - Enables opening Terms of Use and Privacy Policy in external browser
    - Required for App Store compliance

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,10 +9,12 @@ import file_selector_macos
 import in_app_purchase_storekit
 import path_provider_foundation
 import sqflite_darwin
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   InAppPurchasePlugin.register(with: registry.registrar(forPlugin: "InAppPurchasePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
